21430c69b6a3b20a7f2d6383252d0787444aa0ec
Cassidy2024 <157302466+Cassidy2024@users.noreply.github.com>
2024-10-20 20:47:32 +0400

    code solution

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,5 @@
 "use strict";
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
+
 var reviewTotalDisplay = document.querySelector('#reviews');
 var userNameDisplay = document.querySelector('#user');
 var returningUserDisplay = document.querySelector('#returning-user');
